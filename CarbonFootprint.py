98aadf80a7e5385fa0b76476b8c07ebb14891c86
Kradon <kradonzhao@gmail.com>
2020-11-14 12:39:01 -0500

    Remove key
    Removed key to prevent others to use.

--- CarbonFootprint.py
+++ CarbonFootprint.py
@@ -2,15 +2,15 @@ import json
 import requests
 
 
-data = requests.get(" https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyCUwwh5NUdfGua-7zBV2-CJSDAadu1FJz0")
+data = requests.get(" https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=KEY")
 print(data.json())
 
 #conversion values
 ELECTRICTYKWH = .953
-HEATINGOIL = 22.3989
+HEATINGOILGALLON = 22.3989
 NATURALGASS100CUBICFT = 11.7065
-PROPANE = 12.6986
+PROPANEGALLON = 12.6986
 
-UNLEADED = 19.3565
-DIESEL = 22.5092
-
+UNLEADEDGALLON = 19.3565
+DIESELGALLON = 22.5092
+AIRTRAVELPERMILE = 
