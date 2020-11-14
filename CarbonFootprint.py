a3cbd5b2a9675db9d588888bbbf7bb1dc81d671c
Kradon <kradonzhao@gmail.com>
2020-11-14 13:51:53 -0500

    remove key

--- CarbonFootprint.py
+++ CarbonFootprint.py
@@ -2,6 +2,6 @@ import json
 import requests
 import Conversion
 
-data = requests.get(" https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyB8mwGnOAP0eqxFlLXyOsO2BJC6ubBXqQI")
+data = requests.get(" https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=KEY")
 print(data.json())
 
