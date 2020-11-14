43b8dae0e6e5910ec7510de1c871286263835a14
Eren Budur <erenbudur33@gmail.com>
2020-11-14 14:46:08 -0500

    ADD: get_distance function

--- APIFunctions.py
+++ APIFunctions.py
@@ -4,8 +4,12 @@ import requests
 
 class Path:
     def __init__(self, origin,destination):
-        self.origin = origin
-        self.destination = destination
+        self.origin = get_address_id(origin)
+        self.destination = get_address_id(destination)
+        self.distance = get_distance(self.origin,self.destination)
+
+
+    
 
 
 
@@ -14,3 +18,7 @@ def get_address_id(address):
     loc_response = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address="+clean_add + "&key="+"AIzaSyAYZcB1L-QlsdsJSP-gYx7pkPreirPET1Q")
     return loc_response.json()['results'][0]['place_id']
 
+
+def get_distance(start,end):
+    response = requests.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins=place_id:" + start +"&destination=place_id:"+ end + "&key="+"AIzaSyAYZcB1L-QlsdsJSP-gYx7pkPreirPET1Q")
+    return response.json()['rows'][0]["elements"][0]["distance"]["value"]
