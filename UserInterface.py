79979ffa84990ade4f1caccb407acf6cb15e7fb9
Kradon <kradonzhao@gmail.com>
2020-11-14 16:42:01 -0500

    create UserInterface class
    Created a class that makes the interface and then stores the destination and origin input.

--- UserInterface.py
+++ UserInterface.py
@@ -1,24 +1,34 @@
 import PySimpleGUI as sg
+class UserInterface:
+    def __init__(self):
+        self.destination = ''
+        self.origin = ''
 
-layout = [ 
-    [sg.Text('Origin'), sg.InputText()], 
-    [sg.Text('Destination'), sg.InputText()], 
-    [sg.Submit(), sg.Cancel()] 
-] 
+    def Interface(self):
+        layout = [ 
+            [sg.Text('Origin'), sg.InputText()], 
+            [sg.Text('Destination'), sg.InputText()], 
+            [sg.Submit(), sg.Cancel()] 
+        ] 
 
-window = sg.Window('Input Locations', layout)
+        window = sg.Window('Input Locations', layout)
 
 
-while True:
-    event, values = window.read() 
+        while True:
+            event, values = window.read() 
 
-    if event == sg.WINDOW_CLOSED:
-        window.close()
-        break
+            if event == sg.WINDOW_CLOSED:
+                window.close()
+                return -1
     
-    if event == 'Submit':
-        Origin = values[0]
-        Destination = values[1]
-        print(Origin)
-        print(Destination)
-        break
+            if event == 'Submit':
+                if values[0] == '' or values[1] == '':
+                    sg.popup('Please input origin and destination')
+                else:
+                    self.origin = values[0]
+                    self.destination = values[1]
+                    window.close()
+                    return 0
+
+l = UserInterface()
+l.Interface()
