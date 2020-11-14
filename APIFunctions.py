56df67767e8277be6a5766728fb7f021319aa67f
David Sullo <dsullo@bu.edu>
2020-11-14 18:35:42 -0500

    Added a check for the GUI to exit when closing it

--- APIFunctions.py
+++ APIFunctions.py
@@ -1,6 +1,7 @@
 
 import json
 import requests
+import sys
 from UserInterface import UserInterface
 
 class Path:
@@ -9,10 +10,12 @@ class Path:
         
         
         while( True ) :
-            GUI.Interface()
+            bar = GUI.Interface()
+            if bar == -1:
+                sys.exit()
             self.origin= get_address_code(GUI.origin)
             self.destination = get_address_code(GUI.destination)
-            if  self.origin != "No Address Found" and self.destination != "No Address Found":
+            if self.origin != "No Address Found" and self.destination != "No Address Found":
                 break
 
             
@@ -38,6 +41,7 @@ def get_distance_info(start,end):
     clean_end = end.replace('+','%2B')
 
     response = requests.get(f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={clean_start}&destinations={clean_end}&key=AIzaSyAYZcB1L-QlsdsJSP-gYx7pkPreirPET1Q")
+    print(response.json())
     distance = response.json()#['rows'][0]["elements"][0]["distance"]["value"]
     time = response.json()#['rows'][0]["elements"][0]["duration"]["value"]
     return distance, round(time/60.0)
