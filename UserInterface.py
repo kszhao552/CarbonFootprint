95c15a2806ec5bb19d8a5dd0fec70d1fd80ce55b
matt-henriksen <mhenrik@bu.edu>
2020-11-14 17:19:01 -0500

    Added Fuel Economy to UI

--- UserInterface.py
+++ UserInterface.py
@@ -3,11 +3,13 @@ class UserInterface:
     def __init__(self):
         self.destination = ''
         self.origin = ''
+        self.fuelEconomy = ''
 
     def Interface(self):
         layout = [ 
             [sg.Text('Origin'), sg.InputText()], 
-            [sg.Text('Destination'), sg.InputText()], 
+            [sg.Text('Destination'), sg.InputText()],
+            [sg.Text('Fuel Economy'), sg.InputText()],
             [sg.Submit(), sg.Cancel()] 
         ] 
 
@@ -17,16 +19,17 @@ class UserInterface:
         while True:
             event, values = window.read() 
 
-            if event == sg.WINDOW_CLOSED:
+            if event == sg.WINDOW_CLOSED or event == 'Cancel':
                 window.close()
                 return -1
     
             if event == 'Submit':
-                if values[0] == '' or values[1] == '':
-                    sg.popup('Please input origin and destination')
+                if values[0] == '' or values[1] == '' or values[2] == '':
+                    sg.popup('Please input origin and destination and fuel economy')
                 else:
                     self.origin = values[0]
                     self.destination = values[1]
+                    self.fuelEconomy = float(values[2])
                     window.close()
                     return 0
 
