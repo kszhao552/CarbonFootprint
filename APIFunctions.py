cc0dd25aa65c7cd2cb3580c1b8ff9691fb4b511e
Eren Budur <erenbudur33@gmail.com>
2020-11-14 16:47:10 -0500

    ADD: get_distance_info

--- APIFunctions.py
+++ APIFunctions.py
@@ -6,7 +6,7 @@ class Path:
     def __init__(self, origin,destination):
         self.origin = get_address_id(origin)
         self.destination = get_address_id(destination)
-        self.distance = get_distance(self.origin,self.destination)
+        self.distance, self.time = get_distance_info(self.origin,self.destination)
 
 
     
@@ -15,13 +15,16 @@ class Path:
 
 def get_address_id(address):
     clean_add = address.replace(" ","%20")
-    loc_response = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address="+clean_add + "&key="+"AIzaSyAYZcB1L-QlsdsJSP-gYx7pkPreirPET1Q")
-    return loc_response.json()['results'][0]['place_id']
+    loc_response = requests.get(f"https://maps.googleapis.com/maps/api/geocode/json?address={clean_add}&key=AIzaSyAYZcB1L-QlsdsJSP-gYx7pkPreirPET1Q")
+    return loc_response.json()['results'][0]['plus_code']["global_code"]
 
 
-def get_distance(start,end):
-    response = requests.get(f"https://maps.googleapis.com/maps/api/distancematrix/json?origins=place_id:{start}&destination=place_id:{end}&key=AIzaSyAYZcB1L-QlsdsJSP-gYx7pkPreirPET1Q")
-    return response.json()#     ['rows'][0]["elements"][0]["distance"]["value"]
+def get_distance_info(start,end):
+    clean_start = start.replace('+','%2B')
+    clean_end = end.replace('+','%2B')
 
-x = Path("Boston University","Boston Common")
-print(x.distance)
+    response = requests.get(f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={clean_start}&destinations={clean_end}&key=AIzaSyAYZcB1L-QlsdsJSP-gYx7pkPreirPET1Q")
+    distance = response.json()['rows'][0]["elements"][0]["distance"]["value"]
+    time = response.json()['rows'][0]["elements"][0]["duration"]["value"]
+    return distance, round(time/60.0)
+
