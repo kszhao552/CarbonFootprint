b00ceb5787cd9506becd65d26c4966d146327fc3
Eren Budur <erenbudur33@gmail.com>
2020-11-14 23:38:09 -0500

    Final: API

--- APIFunctions.py
+++ APIFunctions.py
@@ -1,4 +1,3 @@
-
 import json
 import requests
 import sys
@@ -42,12 +41,8 @@ def get_address_code(address):
 
 
 def get_distance_info(startlat,startlong,endlat,endlong):
-    
-    response = requests.get(f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={startlat}|{startlong}&destinations={endlat}|{endlong}&key={config.api_key}").json()
-    print(response)
-    distance = response#['rows'][0]["elements"][0]["distance"]["value"]
-    time = response#['rows'][0]["elements"][0]["duration"]["value"]
-    return distance, time #round(time/60.0)
-
-x = Path()
-print(x.distance)
+    print(startlat,startlong,endlat,endlong)
+    response = requests.get(f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={startlat},{startlong}&destinations={endlat},{endlong}&key={config.api_key}").json()
+    distance = response['rows'][0]["elements"][0]["distance"]["value"]
+    time = response['rows'][0]["elements"][0]["duration"]["value"]
+    return distance, round(time/60.0)
