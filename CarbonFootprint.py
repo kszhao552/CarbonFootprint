a02585811c13ac7e7e3410d28eec56f4fbc9746d
matt-henriksen <mhenrik@bu.edu>
2020-11-14 23:43:10 -0500

    Started CarbonFootprint

--- CarbonFootprint.py
+++ CarbonFootprint.py
@@ -1,5 +1,21 @@
 import json
 import requests
 import Conversion
+from UserInterface import UserInterface
+from APIFunctions import Path
+
+path = Path()
+UI = UserInterface()
+path.GUI.SecondInterface
+kwh = path.GUI.kwh
+gallonsPropane = path.GUI.gallonsPropane
+gallonsNatGas = path.GUI.gallonsNatGas
+gallonsHeatOil = path.GUI.gallonsHeatOil
+distance = path.distance
+time = path.time
+fuelEconomy = UI.fuelEconomy
+diesel = UI.diesel
+airDistance = UI.airDistance
+
 
 
