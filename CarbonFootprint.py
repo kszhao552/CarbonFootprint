55e77cb1c51ccc868e6cd0bca2493f5d740de596
David Sullo <dsullo@bu.edu>
2020-11-15 13:14:30 -0500

    Add comparison to national average in CarbonFootprint.py to allow a percentage to be displayed comparing the user's consumption to the average

--- CarbonFootprint.py
+++ CarbonFootprint.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import Conversion
+import Comparisons
 from UserInterface import UserInterface
 from APIFunctions import Path
 import PySimpleGUI as sg
@@ -49,4 +50,10 @@ while True:
     sg.popup(f"Your annual carbon footprint is {totalCarbon} Ibs CO₂/year.\nWith this trip, your carbon footprint will increase by {tripCarbon} Ibs CO₂")
     totalCarbon += tripCarbon
 totalCarbon = round(totalCarbon)
-sg.popup(f"Your annual carbon footprint is\n{totalCarbon} Ibs CO₂/year")
+totalResult = Comparisons.totalFootprintComp(totalCarbon)
+if (totalResult[1] == 1):
+    totalComparison = 'less'
+else:
+    totalComparison = 'more'
+
+sg.popup(f"Your annual carbon footprint is\n{totalCarbon} Ibs CO₂/year.\n Your usage is {totalResult[0]} % {totalComparison} compared to the national average")
