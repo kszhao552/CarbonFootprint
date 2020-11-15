baa33c0d8724607a955b6069e6531ad0a4944f7b
Kradon <kradonzhao@gmail.com>
2020-11-15 09:52:26 -0500

    feat: add interface for only origin and destination.

--- UserInterface.py
+++ UserInterface.py
@@ -85,5 +85,29 @@ class UserInterface:
                     except ValueError:
                         sg.popup('One or more of your inputs is not a number, please try again')
 
-l = UserInterface()
-l.Interface()
+    def ThirdInterface(self):
+        layout = [ 
+            [sg.Text('Origin'), sg.InputText()], 
+            [sg.Text('Destination'), sg.InputText()],
+            [sg.Submit(), sg.Cancel()]
+            ]
+            
+        window = sg.Window('Input Locations', layout)
+
+            
+        while True:
+            event, values = window.read() 
+
+            if event == sg.WINDOW_CLOSED or event == 'Cancel':
+                window.close()
+                return -1
+    
+            if event == 'Submit':
+                if values[0] == '' or values[1] == '' or values[2] == '':
+                    sg.popup('Please input origin and destination and fuel economy')
+                else:
+                        self.origin = values[0]
+                        self.destination = values[1]
+                        window.close()
+                        return 0
+        ]
