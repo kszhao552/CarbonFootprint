519d76679abc4a73acbc6fb81738c8bbac175e22
David Sullo <dsullo@bu.edu>
2020-11-15 00:36:43 -0500

    Started comparisons file and added dummy headers.

---
+++ Comparisons.py
@@ -0,0 +1,29 @@
+# kilowatt hrs. per month
+# gallons of Heating Oil per month
+# 100 cubic feet of natural gas per month
+# gallons of propane per month
+# gallons of diesel driven per month
+# gallons of unleaded driven per month
+# miles flown per year
+# total carbon foot print per year
+
+def kwhComp(userConsumption, natlAverage):
+    return 0
+
+def heatOilComp(userConsumption, natlAverage):
+    return 0
+
+def natGasComp(userConsumption, natlAverage):
+    return 0
+
+def propaneComp(userConsumption, natlAverage):
+    return 0
+
+def gasComp(userConsumption, natlAverage):
+    return 0
+
+def airMilesComp(userConsumption, natlAverage):
+    return 0
+
+def totalFootprintComp(userConsumption, natlAverage):
+    return 0
